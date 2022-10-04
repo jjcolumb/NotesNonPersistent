e0d7c4be9776f827fd86611c50e5ab45b2eec749
Jose Columbie <josejaviercolumbie@yahoo.com>
2022-10-04 16:54:10 -0700

    Add project files.

---
+++ NotesNonPersistent.Module/BusinessObjects/Master.cs
@@ -0,0 +1,83 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace NotesNonPersistent.Module.BusinessObjects
+{
+    [DefaultClassOptions]   
+    public class Master : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Master(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        string _name;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => _name;
+            set => SetPropertyValue(nameof(Name), ref _name, value);
+        }
+
+        bool _activo;
+        public bool Activo
+        {
+            get => _activo;
+            set => SetPropertyValue(nameof(Activo), ref _activo, value);
+        }
+
+        string _notesNonPersistent;
+        [Size(SizeAttribute.Unlimited)]
+        [NonPersistent]
+        public string NotesNonPersistent
+        {
+            get => _notesNonPersistent;
+            set => SetPropertyValue(nameof(NotesNonPersistent), ref _notesNonPersistent, value);
+        }
+
+        [Association("Master-Notes")]
+        public XPCollection<Notes> Notes
+        {
+            get
+            {
+                return GetCollection<Notes>(nameof(Notes));
+            }
+        }
+
+
+        protected override void OnSaving()
+        {
+            base.OnSaving();
+
+            if (!String.IsNullOrEmpty(NotesNonPersistent))
+            {
+                var note = new Notes(Session);
+                note.Text = NotesNonPersistent;
+                note.DateCreated = DateTime.Now;
+                note.User = Session.GetObjectByKey<ApplicationUser>(SecuritySystem.CurrentUserId);
+                note.Master = this;
+                note.Save();
+                NotesNonPersistent = String.Empty;
+            }
+
+        }
+    }
+}
---
+++ NotesNonPersistent.Module/BusinessObjects/Notes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using DevExpress.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+
+namespace NotesNonPersistent.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Notes : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Notes(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        string _text;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Text
+        {
+            get => _text;
+            set => SetPropertyValue(nameof(Text), ref _text, value);
+        }
+
+        DateTime _dateCreated;
+        public DateTime DateCreated
+        {
+            get => _dateCreated;
+            set => SetPropertyValue(nameof(DateCreated), ref _dateCreated, value);
+        }
+
+        ApplicationUser _user;
+        public ApplicationUser User
+        {
+            get => _user;
+            set => SetPropertyValue(nameof(User), ref _user, value);
+        }
+
+        Master _master;
+        [Association("Master-Notes")]
+        public Master Master
+        {
+            get => _master;
+            set => SetPropertyValue(nameof(Master), ref _master, value);
+        }
+
+    }
+}
